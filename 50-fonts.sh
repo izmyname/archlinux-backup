3497f855604a825cd683a05d2e6630ea4e42b7a8
Izmyname <VI-MCMXCV@proton.me>
2024-12-01 22:24:18 +0500

    general:upload

---
+++ 50-fonts.sh
@@ -0,0 +1,9 @@
+# install
+AddPackage adobe-source-han-sans-cn-fonts # Adobe Source Han Sans Subset OTF - Simplified Chinese OpenType/CFF fonts
+AddPackage adobe-source-han-sans-jp-fonts # Adobe Source Han Sans Subset OTF - Japanese OpenType/CFF fonts
+AddPackage adobe-source-han-sans-kr-fonts # Adobe Source Han Sans Subset OTF - Korean OpenType/CFF fonts
+AddPackage noto-fonts                     # Google Noto TTF fonts
+AddPackage noto-fonts-emoji               # Google Noto emoji fonts
+AddPackage otf-font-awesome               # Iconic font designed for Bootstrap
+AddPackage ttf-fira-code                  # Monospaced font with programming ligatures
+AddPackage ttf-firacode-nerd              # Patched font Fira (Fura) Code from nerd fonts library
---
+++ 70-misc.sh
@@ -0,0 +1,6 @@
+# stuff
+SetFileProperty /etc/kernel/install.d deleted y
+SetFileProperty /etc/kernel deleted y
+SetFileProperty /usr/bin/groupmems group groups
+SetFileProperty /usr/bin/groupmems mode 2750
+SetFileProperty /usr/bin/mullvad-exclude mode 4755
