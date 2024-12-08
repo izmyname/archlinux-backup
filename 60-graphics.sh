97113b767778975eaef46995bdfb455a5e53f7cf
Izmyname <VI-MCMXCV@proton.me>
2024-12-08 22:58:05 +0500

    temporary use waybar-git until 0.12 releases

--- 60-graphics.sh
+++ 60-graphics.sh
@@ -1,27 +1,28 @@
 # packages
-AddPackage adw-gtk-theme                      # Unofficial GTK 3 port of the libadwaita theme
-AddPackage brightnessctl                      # Lightweight brightness control tool
-AddPackage chafa                              # Image-to-text converter supporting a wide range of symbols and palettes, transparency, animations, etc.
-AddPackage chezmoi                            # Manage your dotfiles across multiple machines
-AddPackage cliphist                           # wayland clipboard manager
-AddPackage dconf-editor                       # GSettings editor for GNOME
-AddPackage ffmpegthumbnailer                  # Lightweight video thumbnailer that can be used by file managers
-AddPackage foot                               # Fast, lightweight, and minimalistic Wayland terminal emulator
-AddPackage foot-terminfo                      # Extra non-standard terminfo files for foot, a Wayland terminal emulator
-AddPackage fuzzel                             # Application launcher for wlroots based Wayland compositors
-AddPackage grim                               # Screenshot utility for Wayland
-AddPackage kvantum                            # SVG-based theme engine for Qt6 (including config tool and extra themes)
-AddPackage mako                               # Lightweight notification daemon for Wayland
-AddPackage mesa-utils                         # Essential Mesa utilities
-AddPackage mpv                                # a free, open source, and cross-platform media player
-AddPackage papirus-icon-theme                 # Papirus icon theme
-AddPackage qt6ct                              # Qt 6 Configuration Utility
-AddPackage slurp                              # Select a region in a Wayland compositor
-AddPackage swayimg                            # A lightweight image viewer for Wayland display servers
-AddPackage udiskie                            # Removable disk automounter using udisks
-AddPackage vkd3d                              # Direct3D 12 to Vulkan translation library By WineHQ
-AddPackage vulkan-tools                       # Vulkan Utilities and Tools
-AddPackage waybar                             # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage adw-gtk-theme      # Unofficial GTK 3 port of the libadwaita theme
+AddPackage brightnessctl      # Lightweight brightness control tool
+AddPackage chafa              # Image-to-text converter supporting a wide range of symbols and palettes, transparency, animations, etc.
+AddPackage chezmoi            # Manage your dotfiles across multiple machines
+AddPackage cliphist           # wayland clipboard manager
+AddPackage dconf-editor       # GSettings editor for GNOME
+AddPackage ffmpegthumbnailer  # Lightweight video thumbnailer that can be used by file managers
+AddPackage foot               # Fast, lightweight, and minimalistic Wayland terminal emulator
+AddPackage foot-terminfo      # Extra non-standard terminfo files for foot, a Wayland terminal emulator
+AddPackage fuzzel             # Application launcher for wlroots based Wayland compositors
+AddPackage grim               # Screenshot utility for Wayland
+AddPackage kvantum            # SVG-based theme engine for Qt6 (including config tool and extra themes)
+AddPackage mako               # Lightweight notification daemon for Wayland
+AddPackage mesa-utils         # Essential Mesa utilities
+AddPackage mpv                # a free, open source, and cross-platform media player
+AddPackage papirus-icon-theme # Papirus icon theme
+AddPackage qt6ct              # Qt 6 Configuration Utility
+AddPackage slurp              # Select a region in a Wayland compositor
+AddPackage swayimg            # A lightweight image viewer for Wayland display servers
+AddPackage udiskie            # Removable disk automounter using udisks
+AddPackage vkd3d              # Direct3D 12 to Vulkan translation library By WineHQ
+AddPackage vulkan-tools       # Vulkan Utilities and Tools
+#AddPackage waybar                             # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage --foreign waybar-git               # temporary use git version, untill 0.12 release with hyprland-fullscreen indicator fix
 AddPackage xdg-desktop-portal-gtk             # A backend implementation for xdg-desktop-portal using GTK
 AddPackage --foreign catppuccin-cursors-mocha # Soothing pastel mouse cursors - Mocha
 AddPackage --foreign hyprland-meta-git        # Meta package to install Hyprland and all related utilities
